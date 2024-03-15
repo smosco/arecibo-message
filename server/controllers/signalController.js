aaab810656dde1ed4cea14a909446ac516cc7f9e
smosco <go1ruf2tk3@gmail.com>
2024-03-16 01:21:53 +0900

    feat: 에러를 처리하는 커스텀 미들웨어 추가

--- server/controllers/signalController.js
+++ server/controllers/signalController.js
@@ -1,63 +1,72 @@
+const asyncHandler = require('express-async-handler');
 const Signal = require('../models/signalModel');
 
-const createSignal = async (req, res) => {
+const createSignal = asyncHandler(async (req, res) => {
   try {
     const signal = await Signal.create(req.body);
     res.status(200).json(signal);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
-const getSignals = async (req, res) => {
+const getSignals = asyncHandler(async (req, res) => {
   try {
     const signals = await Signal.find({});
     res.status(200).json(signals);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
-const getSignal = async (req, res) => {
+const getSignal = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const signal = await Signal.findById(id);
     res.status(200).json(signal);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
-const updateSignal = async (req, res) => {
+const updateSignal = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const signal = await Signal.findByIdAndUpdate(id, req.body);
     if (!signal) {
       return res
         .status(404)
-        .json({ message: `cannot find any signal with ID ${id}` });
+        .json({ message: `ID ${id}에 해당하는 신호를 찾을 수 없어요` });
+      // TODO: 미들웨어 사용시 404가 아닌 500이 뜨는 문제
+      //   throw new Error(`ID ${id}에 해당하는 신호를 찾을 수 없어요`);
     }
     const updatedSignal = await Signal.findById(id);
     res.status(200).json(updatedSignal);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
-const deleteSignal = async (req, res) => {
+const deleteSignal = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const signal = await Signal.findByIdAndDelete(id);
     if (!signal) {
       return res
         .status(404)
-        .json({ message: `cannot find any signal with ID ${id}` });
+        .json({ message: `ID ${id}에 해당하는 신호를 찾을 수 없어요` });
+      //   throw new Error(`ID ${id}에 해당하는 신호를 찾을 수 없어요`);
     }
     res.status(200).json(signal);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500);
+    throw new Error(error.message);
   }
-};
+});
 
 module.exports = {
   createSignal,
