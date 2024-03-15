d4d02508222bf00a1c61cf317002bc42cd596a8c
smosco <go1ruf2tk3@gmail.com>
2024-03-16 01:25:09 +0900

    feat: 다른 출처의 리소스 요청 처리할 수 있도록 CORS 미들웨어 사용

--- server/server.js
+++ server/server.js
@@ -2,14 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const signalRoute = require('./routes/signalRoute');
+const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
+const FRONTEND = process.env.FRONTEND;
 
+const corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200,
+};
+
+// 미들웨어의 순서가 중요
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/signals', signalRoute);
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Hello SETI');
