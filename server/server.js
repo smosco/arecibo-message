fc6faf84b2f80b8f2a8106d69a8fef79b60c723d
smosco <go1ruf2tk3@gmail.com>
2024-03-16 00:55:48 +0900

    chore: Mongoose를 사용하여 MongoDB 연결

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 app.get('/', (req, res) => {
   res.send('Hello SETI');
 });
 
-app.listen(8080, () => {
-  console.log('seti server is running on port 8080');
-});
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log('seti server is running on port 8080');
+    });
+    console.log('connected mongodb');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
