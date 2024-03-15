fa11ab1230f7cd0c8748cd683e7b3f086ff08656
smosco <go1ruf2tk3@gmail.com>
2024-03-16 00:58:34 +0900

    feat: 아레시보 메세지 데이터를 저장하기 위한 Mongoose 모델 생성

---
+++ server/models/signalModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const signalSchema = mongoose.Schema(
+  {
+    binaryCode: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Signal = mongoose.model('Signal', signalSchema);
+
+module.exports = Signal;
