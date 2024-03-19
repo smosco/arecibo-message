f6fcb1ed369e6aa10a74b2094ef13513be523bdf
smosco <go1ruf2tk3@gmail.com>
2024-03-19 12:58:35 +0900

    feat: react-router 설정 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/create',
+    element: <CreatePage />,
+  },
+]);
 
 const App = () => {
-  return <div>SETI</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
