c85c0966455396fa07bef2cf4030631cb0294e25
smosco <go1ruf2tk3@gmail.com>
2024-03-19 12:32:32 +0900

    chore: eslint, prettier 설정

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,9 +10,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
